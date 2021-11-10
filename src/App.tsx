544388f84abcafa3dd14e1dbbd3e73a22797c4ae
Wojciech <wojtekszutryk@gmail.com>
2021-11-10 20:23:20 +0100

    add select station name component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,60 @@
-import React, {useEffect, useMemo} from 'react';
-import './App.css';
-import {satelliteStationsBaseToNames} from "./functions/sateliteStationsBaseToNames";
-import {useAppSelector} from "./functions/hooks/useAppSelector";
-import {useAppDispatch} from "./functions/hooks/useAppDispach";
-import {setStationsSatellitesNames} from "./state/actions";
-import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import React, { useEffect, useMemo } from 'react'
+import './App.css'
+import { satelliteStationsBaseToNames } from './functions/sateliteStationsBaseToNames'
+import { useAppSelector } from './functions/hooks/useAppSelector'
+import { useAppDispatch } from './functions/hooks/useAppDispach'
+import {
+    setSelectedStationsSatellitesName,
+    setStationsSatellitesNames,
+} from './state/actions'
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+} from '@mui/material'
 
 function App() {
-  const dispatch = useAppDispatch()
-  const names = useAppSelector((state) => state.app.stationsSatellitesNames)
-  useEffect(()=>{
-    satelliteStationsBaseToNames().then(r => dispatch(setStationsSatellitesNames(r)))
-  },[dispatch])
-    const selectNameOptions = useMemo(() =>{
-        return names.map((name:string) => (
-            <MenuItem key={name} value={name}>{name}</MenuItem>
+    const dispatch = useAppDispatch()
+    const names = useAppSelector((state) => state.app.stationsSatellitesNames)
+    const selectedName = useAppSelector(
+        (state) => state.app.selectedStationsSatellitesName
+    )
+    useEffect(() => {
+        satelliteStationsBaseToNames().then((r) =>
+            dispatch(setStationsSatellitesNames(r))
+        )
+    }, [dispatch])
+    const selectNameOptions = useMemo(() => {
+        return names.map((name: string) => (
+            <MenuItem key={name} value={name}>
+                {name}
+            </MenuItem>
         ))
-    },[names])
-  return (
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Station or Satellite</InputLabel>
-        <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            // value={age}
-            label="Age"
-            // onChange={handleChange}
-        >
-            {selectNameOptions}
-        </Select>
-      </FormControl>
-  );
+    }, [names])
+
+    const handleChange = (event: SelectChangeEvent) => {
+        dispatch(
+            setSelectedStationsSatellitesName(event.target.value as string)
+        )
+    }
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+                Station or Satellite
+            </InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedName}
+                label="Age"
+                onChange={handleChange}
+            >
+                {selectNameOptions}
+            </Select>
+        </FormControl>
+    )
 }
 
-export default App;
+export default App
