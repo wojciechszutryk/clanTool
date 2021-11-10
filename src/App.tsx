2ebb8f3163b4cfee1cdbbaf73cfef762539cea21
Wojciech <wojtekszutryk@gmail.com>
2021-11-10 19:12:32 +0100

    add redux, add satelliteStationsBaseToNames function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {satelliteStationsBaseToNames} from "./functions/sateliteStationsBaseToNames";
+import {useAppSelector} from "./functions/hooks/useAppSelector";
+import {useAppDispatch} from "./functions/hooks/useAppDispach";
+import {setStationsSatellitesNames} from "./state/actions";
 
 function App() {
+  const dispatch = useAppDispatch()
+  const names = useAppSelector((state) => state.app)
+  useEffect(()=>{
+    satelliteStationsBaseToNames().then(r => dispatch(setStationsSatellitesNames(r)))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
