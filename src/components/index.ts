8ce3224c008769a88088f6286c6e3da8cf4987dc
Wojciech <wojtekszutryk@gmail.com>
2021-11-17 20:05:50 +0100

    fix rorigin repo

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as SatelliteSelect } from './SatelliteSelect'
+export { default as StationSelect } from './StationSelect'
+export { default as DrawChart } from './DrawChart'
+export { default as MyGoogleMaps } from './MyGoogleMap'
