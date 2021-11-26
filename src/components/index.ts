23ba6596fd7c5dddc213fd76cabc2c4fec56fb9e
Wojciech <wojtekszutryk@gmail.com>
2021-11-26 22:07:29 +0100

    merge dev branch

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as SatelliteSelect } from './SatelliteSelect'
 export { default as StationSelect } from './StationSelect'
-export { default as MyGoogleMaps } from './MyGoogleMap'
 export { default as MyMaps } from './MyMaps'
 export { default as DatePicker } from './DatePicker'
 export { default as DataChart } from './DataChart'
