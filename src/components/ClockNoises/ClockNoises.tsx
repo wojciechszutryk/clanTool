13e628e77cdb534949ec25a014a0e4f159a41370
wojtek <wojtek.szutryk@gmail.com>
2022-02-18 22:36:48 +0100

    fix aParameter calculation in clockNoises

--- src/components/ClockNoises/ClockNoises.tsx
+++ src/components/ClockNoises/ClockNoises.tsx
@@ -6,21 +6,19 @@ const ClockNoises = ({data}: {data: {[key: string]: { x: number; y: number }[]}[
     const squres = useMemo(()=> {
         return data.map(devObj => (
             <Box key={Object.keys(devObj)[0]}>
-                {Object.values(devObj).map((xyValueArray) => (
-                    xyValueArray.map((xyValue, index) => {
+                {Object.values(devObj).map((xyValueArray) => {
+                    const yValuesArr = xyValueArray.map(obj => obj.y);
+                    const deltaY = (Math.max(...yValuesArr) - Math.min(...yValuesArr))/(yValuesArr.length - 1)
+                    return xyValueArray.map((xyValue, index) => {
                         if(index === xyValueArray.length-1) return null;
-
-                        //nie działa bo skale w x i y są nie takie same + po x zwiększa sie niereularnie
-                        console.log(xyValueArray[index+1].y - xyValueArray[index].y);
-                        
-                        const aParameter = (xyValueArray[index+1].y - xyValueArray[index].y) / (xyValueArray[index+1].x - xyValueArray[index].x)
+                        const aParameter = (xyValueArray[index+1].y - xyValueArray[index].y) / deltaY
                         return (
                             <Tooltip key={index} title="Add" placement="top-start">
                                 <Box>{aParameter}</Box>
                             </Tooltip>
                         )
                     })
-                ))}
+                })}
             </Box>
         ))
         // if(data.length === 0) return [];
