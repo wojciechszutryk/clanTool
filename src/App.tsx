ed38fcc48250fe803c41350357e309543c64b7be
w.szutryk <w.szutryk@optimumparto.com>
2023-04-06 08:57:13 +0200

    routes refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import InfoIcon from '@mui/icons-material/Info'
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import { toast, ToastContainer } from 'react-toastify/dist'
 import { Footer } from './components/Footer'
-import AboutPage from './pages/AboutPage'
-import InstructionsPage from './pages/InstructionsPage'
-import StationsPage from './pages/StationsPage'
 import { StyledAppContent, StyledAppWrapper } from './styles'
 import { Header } from './components/Header'
-import SatellitesPage from './pages/SatellitesPage'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+const SatellitesPage = lazy(() => import('./pages/ChartsPages/SatellitesPage'))
+const StationsPage = lazy(() => import('./pages/ChartsPages/StationsPage'))
+const InstructionsPage = lazy(() => import('./pages/InstructionsPage'))
+const AboutPage = lazy(() => import('./pages/AboutPage'))
 
 function App() {
     toast.configure()
@@ -21,17 +22,40 @@ function App() {
             <StyledAppWrapper>
                 <StyledAppContent>
                     <Routes>
-                        <Route
-                            path="*"
-                            element={<Navigate to="/satellites" replace />}
-                        />
-                        <Route path="satellites" element={<SatellitesPage />} />
-                        <Route path="stations" element={<StationsPage />} />
-                        <Route
-                            path="instructions"
-                            element={<InstructionsPage />}
-                        />
-                        <Route path="about" element={<AboutPage />} />
+                        <Route path="/">
+                            <Route
+                                path="*"
+                                element={
+                                    <Suspense fallback={null}>
+                                        <SatellitesPage />
+                                    </Suspense>
+                                }
+                            />
+                            <Route
+                                path="stations"
+                                element={
+                                    <Suspense fallback={null}>
+                                        <StationsPage />
+                                    </Suspense>
+                                }
+                            />
+                            <Route
+                                path="instructions"
+                                element={
+                                    <Suspense fallback={null}>
+                                        <InstructionsPage />
+                                    </Suspense>
+                                }
+                            />
+                            <Route
+                                path="about"
+                                element={
+                                    <Suspense fallback={null}>
+                                        <AboutPage />
+                                    </Suspense>
+                                }
+                            />
+                        </Route>
                     </Routes>
                 </StyledAppContent>
             </StyledAppWrapper>
